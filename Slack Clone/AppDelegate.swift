6f60bc271fa53330d51fc474fb7197082b556050
Jason Wong <jawong71@gmail.com>
2018-06-23 21:30:16 -0700

    Initial Commit

---
+++ Slack Clone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Slack Clone
+//
+//  Created by Jason Wong on 2018-06-23.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
